e17cdf5b425e9b0fd9d5d0d7748ff1995a119276
Rupen <rupen@yahoo.com>
2021-09-04 00:09:52 +0100

    message

---
+++ src/components/CustomBtn.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import {withStyles} from '@material-ui/core/styles';
+
+
+const StyledButton = withStyles({
+    root: {
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "44px",
+        padding: "0 25px",
+        boxSizing: "border-box",
+        borderRadius: 0, 
+        background: "#00a650",
+        color: "#fff",
+        transform: "none",
+        boxShadow: "6px 6px 0 0 #c7d8ed",
+        transition: "background .3s,border-color .3s,color .3s",
+        "&:hover": {
+            backgroundColor:  "#00a650",
+            color: "#ff4000",
+            transform: "translate(2%,2%)"
+        },
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+})(Button);
+
+
+export default function NavBar(props) {
+    return (
+        <StyledButton variant="contained">{props.txt}</StyledButton>
+    )
+}
